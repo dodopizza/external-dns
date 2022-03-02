0e846a8ecaa47c1b8f525103154db9e811731f60
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-03-02 21:43:24 +0300

    feat(yandex): add empty types & use provider in main
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- main.go
+++ main.go
@@ -68,6 +68,7 @@ import (
 	"sigs.k8s.io/external-dns/provider/ultradns"
 	"sigs.k8s.io/external-dns/provider/vinyldns"
 	"sigs.k8s.io/external-dns/provider/vultr"
+	"sigs.k8s.io/external-dns/provider/yandex"
 	"sigs.k8s.io/external-dns/registry"
 	"sigs.k8s.io/external-dns/source"
 )
@@ -327,6 +328,14 @@ func main() {
 		p, err = gandi.NewGandiProvider(ctx, domainFilter, cfg.DryRun)
 	case "safedns":
 		p, err = safedns.NewSafeDNSProvider(domainFilter, cfg.DryRun)
+	case "yandex":
+		p, err = yandex.NewYandexProvider(
+			&yandex.YandexConfig{
+				DomainFilter:   domainFilter,
+				ZoneNameFilter: zoneNameFilter,
+				DryRun:         cfg.DryRun,
+			},
+		)
 	default:
 		log.Fatalf("unknown dns provider: %s", cfg.Provider)
 	}
